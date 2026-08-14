6b762d3c037ab3f28a3e1dc9d5b2fa8f50a1f491
Remi Vansteelandt <remvst@gmail.com>
2026-08-14 18:01:17 -0400

    Kewl bike

--- src/entities/bike.js
+++ src/entities/bike.js
@@ -2,6 +2,8 @@ class Bike extends PhysicsObject {
     constructor() {
         super();
 
+        this.categories.push('bike');
+
         // TODO
         // this.lastBackWheelOnGround = 0;
         // this.lastBackWheelSegment = null;
@@ -18,5 +20,38 @@ class Bike extends PhysicsObject {
         head.position.y = -20;
         head.radius = 5;
 
+    }
+
+    cycle(elapsed) {
+        super.cycle(elapsed);
+
+        const raiseWheel = downKeys[37];
+        const lowerWheel = downKeys[39];
+        const accelerate = downKeys[38];
+
+        if (raiseWheel) this.momentum.rotation -= elapsed * Math.PI * 2;
+        if (lowerWheel) this.momentum.rotation += elapsed * Math.PI * 2;
+        if (accelerate && this.age - this.lastBackWheelOnGround < 0.1) {
+            // TODO check that the back wheel is in contact with the ground
+            // TODO momentum should be based off the curve of the ground
+            // TODO update rotation momentum as well
+
+            // Base momentum off the curve
+            let sdx = this.lastBackWheelSegment.p2.x - this.lastBackWheelSegment.p1.x;
+            let sdy = this.lastBackWheelSegment.p2.y - this.lastBackWheelSegment.p1.y;
+            // Force direction to always point rightward
+            if (sdx < 0) { sdx = -sdx; sdy = -sdy; }
+            const slen = Math.hypot(sdx, sdy);
+            sdx /= slen; sdy /= slen;
+
+            // Cross product: positive means wheel is below the segment (ceiling contact)
+            const backWheel = this.absolute(this.hitboxes[1], new Hitbox());
+            const cross = sdx * (backWheel.position.y - this.lastBackWheelSegment.p1.y)
+                        - sdy * (backWheel.position.x - this.lastBackWheelSegment.p1.x);
+            const sign = cross > 0 ? -1 : 1;
+
+            this.momentum.position.x += sign * sdx * elapsed * 20;
+            this.momentum.position.y += sign * sdy * elapsed * 20;
+        }
     }
 }
--- src/entities/ground.js
+++ src/entities/ground.js
@@ -12,7 +12,7 @@ class Ground extends Entity {
         ctx.strokeStyle = 'red';
         ctx.beginPath();
         for (let x = camera.position.x - CANVAS_WIDTH / 2 ; x < camera.position.x + CANVAS_WIDTH / 2 ; x += 5) {
-            ctx.lineTo(x, Math.sin(x * PI * 2 / 200) * 100);
+            ctx.lineTo(x, this.curveAt(x));
         }
         ctx.stroke();
 
@@ -23,11 +23,11 @@ class Ground extends Entity {
 
     getSegments() {
         // TODO use the player as a ref
-        const camera = firstItem(this.world.category('camera'));
+        const bike = firstItem(this.world.category('bike'));
 
         const stepX = 20;
         const window = 200;
-        const refX = floorToNearest(camera.position.x, stepX);
+        const refX = floorToNearest(bike.position.x, stepX);
 
         return this.segments.getOrCreate(
             floorToNearest(refX, stepX * 2),
@@ -45,6 +45,6 @@ class Ground extends Entity {
     }
 
     curveAt(x) {
-        return Math.sin(x * PI * 2 / 200) * 100;
+        return Math.sin(x * PI * 2 / 400) * 50 + 100;
     }
 }
