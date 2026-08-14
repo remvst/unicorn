1d0d08131c1d57045a253e34375808b680b0caa5
Remi Vansteelandt <remvst@gmail.com>
2026-08-14 19:11:11 -0400

    Improve curves

--- src/entities/ground.js
+++ src/entities/ground.js
@@ -1,7 +1,8 @@
 class PerlinCurve {
-    constructor(step, amplitude) {
-        this.step = step;
-        this.amplitude = amplitude;
+    constructor(opts) {
+        this.step = opts.step ?? 100;
+        this.amplitude = opts.amplitude ?? 0;
+        this.plus = opts.plus ?? [];
         this.seeds = [];
         for (let i = 0 ; i < 50 ; i++) {
             this.seeds.push(Math.random());
@@ -9,6 +10,8 @@ class PerlinCurve {
     }
 
     yFor(x) {
+        const plus = this.plus.reduce((acc, plus) => acc + plus.yFor(x), 0);
+
         const index = floor(x / this.step);
         const ratio = x / this.step - index;
 
@@ -18,7 +21,7 @@ class PerlinCurve {
         const gradientBefore = (Math.cos(before * Math.PI * 2) + Math.sin(before * Math.PI * 2)) * ratio;
         const gradientAfter = (Math.cos(after * Math.PI * 2) + Math.sin(after * Math.PI * 2)) * (ratio - 1);
 
-        return interpolate(
+        return plus + interpolate(
             gradientBefore * this.amplitude,
             gradientAfter * this.amplitude,
             smoothstep(ratio),
@@ -32,7 +35,12 @@ class Ground extends Entity {
         super();
         this.categories.push('ground');
         this.segments = new Cache();
-        this.curve = new PerlinCurve(500, 100);
+
+        this.curve = new PerlinCurve({ plus: [
+            new PerlinCurve({ step: 2000, amplitude: 800 }),
+            new PerlinCurve({ step: 500, amplitude: 200 }),
+            // new PerlinCurve({ step: 200, amplitude: 20 }),
+        ], shiftY: 100 });
     }
 
     render() {
@@ -54,8 +62,8 @@ class Ground extends Entity {
         // TODO use the player as a ref
         const bike = firstItem(this.world.category('bike')) || firstItem(this.world.category('camera'));
 
-        const stepX = 20;
-        const window = 200;
+        const stepX = 50;
+        const window = 400;
         const refX = floorToNearest(bike.position.x, stepX);
 
         return this.segments.getOrCreate(
@@ -74,7 +82,7 @@ class Ground extends Entity {
     }
 
     curveAt(x) {
-        return this.curve.yFor(x) + 100;
+        return this.curve.yFor(x);
         return x > 100 && x < 500 ? 200 : Math.sin(x * PI * 2 / 400) * 50 + 100;
     }
 }
