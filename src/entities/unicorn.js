b14fda78387be5c51eb97d7de6e0a20894191436
Remi Vansteelandt <remvst@gmail.com>
2026-08-15 22:24:11 -0400

    Early unicorn rendering

--- src/entities/unicorn.js
+++ src/entities/unicorn.js
@@ -1,8 +1,20 @@
 class Unicorn extends Entity {
+
+    constructor() {
+        super();
+        this.renderable = new UnicornRenderable();
+    }
+
     render() {
         ctx.translate(this.position.x, this.position.y);
-        ctx.fillStyle = '#fff';
+        ctx.fillStyle = '#f00';
         ctx.fillRect(-10, -10, 20, 20);
+
+        // ctx.fillRect();
+
+        this.renderable.render();
+
+
     }
 
     cycle(elapsed) {
@@ -12,5 +24,49 @@ class Unicorn extends Entity {
             this.position.x = camera.position.x + CANVAS_WIDTH + random(500, 1000);
             this.position.y = ground.curveAt(this.position.x);
         }
+    }
+}
+
+class UnicornRenderable extends SkeletonRenderable {
+    constructor() {
+        super();
+
+        this.butt = {
+            x: -20,
+            y: 0,
+        }
+
+        this.shoulders = {
+            x: 20,
+            y: 0,
+        };
+
+        this.leftFrontFoot = {
+            x: this.shoulders.x,
+            y: this.shoulders.y + 20,
+        };
+
+        this.leftBackFoot = {
+            x: this.butt.x,
+            y: this.shoulders.y + 20,
+        };
+
+        this.head = {
+            x: this.shoulders.x + 10,
+            y: this.shoulders.y - 10,
+        };
+
+        this.nose = {
+            x: this.head.x + 5,
+            y: this.head.y + 5,
+        };
+
+        this
+            .add(lineRenderable(this.butt, this.shoulders, 4))
+            .add(lineRenderable(this.shoulders, this.leftFrontFoot, 4))
+            .add(lineRenderable(this.butt, this.leftBackFoot, 4))
+            .add(lineRenderable(this.shoulders, this.head, 4))
+            .add(lineRenderable(this.head, this.nose, 4))
+            ;
     }
 }
