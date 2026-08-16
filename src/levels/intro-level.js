5ec5434023521c5f43f6a1511f5ca95be90e70c6
Remi Vansteelandt <remvst@gmail.com>
2026-08-16 19:36:35 -0400

    Throwaway autopilot

--- src/levels/intro-level.js
+++ src/levels/intro-level.js
@@ -2,13 +2,22 @@ class IntroLevel extends Level {
     async setup() {
         this.transitionIntoCurve(new PerlinCurve({ plus: [
             // new PerlinCurve({ step: 2000, amplitude: 800 }),
-            new PerlinCurve({ step: 500, amplitude: 200 }),
+            new PerlinCurve({ step: 700, amplitude: 400 }),
             // new PerlinCurve({ step: 200, amplitude: 80, multiplier: x => abs(sin(x / 2000)) }),
         ] }));
 
         const { camera } = this.basics();
-        camera.zoom = 2;
+        camera.zoom = 1.5;
         camera.offset = 0;
+
+        // this.world.remove(firstItem(this.world.category('player')));
+        // this.world.remove(firstItem(this.world.category('hud')));
+
+        // const { ground } = this.basics();
+
+        // const bike = this.world.add(new AutopilotBike());
+        // bike.position.y = ground.curveAt(bike.position.x) -
+        //     bike.backWheel.position.y - bike.backWheel.radius; // TODO eventually hardcode this?
 
         await new Promise((r) => {});
     }
