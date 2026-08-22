8617ddf0f6040a358f13c0e8101585838087b014
Remi Vansteelandt <remvst@gmail.com>
2026-08-22 14:34:06 -0400

    Fix wheelie tutorial

--- src/levels/tutorial-wheelies.js
+++ src/levels/tutorial-wheelies.js
@@ -1,18 +1,23 @@
 class TutorialWheelies extends Level {
     async setup() {
         await this.levelTransition({
-            dialog: [
-                'Okay, here\'s the tricky part',
-                'Balance yourself on one wheel and you\'ve got a wheelie',
-                'Let\'s see 3 of those',
-            ],
+            transition: (x) => this.runUnicornDialog(x, [
+                nomangle('Aight this next part is fairly advanced'),
+                nomangle('If you balance yourself on one wheel, that\'s called a wheelie'),
+                nomangle('Wheelies allow you to connect combos together'),
+            ]),
             curve: plains(),
         });
+
         this.world.addUnique(new Prompt('◄ TO BALANCE ON ONE WHEEL'));
 
         await this.runObjectives({
             objectives: [
-                new Objective('perform 3 wheelies'.toUpperCase(), 3, () => awaitTrick(this.world, t => t.label.toLowerCase().includes('wheelie'))),
+                new Objective(
+                    nomangle('DO A WHEELIE'),
+                    3,
+                    () => awaitTrick(this.world, anyWheelie),
+                ),
             ],
         });
 
