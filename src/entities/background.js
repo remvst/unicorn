a1db6ae7610559cfa74b80bbeda46592b2ad10a4
Remi Vansteelandt <remvst@gmail.com>
2026-08-23 17:24:09 -0400

    Tiny format change

--- src/entities/background.js
+++ src/entities/background.js
@@ -20,15 +20,19 @@ class Background extends Entity {
         const camera = firstItem(this.world.category(Camera));
 
         ctx.wrap(() => {
+            this.cancelCameraTransformations();
+            ctx.globalAlpha = this.alpha;
+
             ctx.fillStyle = (this.bgGradient ||= (() => {
                 const gradient = ctx.createLinearGradient(0, CANVAS_HEIGHT, CANVAS_WIDTH, 0);
                 for (let i = 0; i < BACKGROUND_COLOR_COUNT; i++) {
-                    gradient.addColorStop(i / BACKGROUND_COLOR_COUNT, BG_COLORS[~~(i + this.colorIndex) % BG_COLORS.length]);
+                    gradient.addColorStop(
+                        i / BACKGROUND_COLOR_COUNT,
+                        BG_COLORS[~~(i + this.colorIndex) % BG_COLORS.length],
+                    );
                 }
                 return gradient;
             })());
-            ctx.globalAlpha = this.alpha;
-            ctx.translate(camera.position.x - CANVAS_WIDTH / 2, camera.position.y - CANVAS_HEIGHT / 2)
             ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
         });
 
