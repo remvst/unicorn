6c05bebc56f43347360589ee43e76e84fe72957d
Remi Vansteelandt <remvst@gmail.com>
2026-08-24 22:34:07 -0400

    Don't need !! for jump

--- src/input/controls.js
+++ src/input/controls.js
@@ -3,7 +3,7 @@ updateControls = controls => {
     if (downKeys[37]) controls.spin--;
     if (downKeys[39]) controls.spin++;
     controls.spin ||= gamepadAxisValue(0);
-    controls.jump = downKeys[32] || !!gamepadButtonValue(0);
+    controls.jump = downKeys[32] || gamepadButtonValue(0);
     controls.brake = downKeys[40] || gamepadButtonValue(6);
     controls.accelerate = downKeys[38] || gamepadButtonValue(7);
 };
