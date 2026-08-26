ae42f7ae6ebfa80a96f61bd26f5468cb672cd857
Remi Vansteelandt <remvst@gmail.com>
2026-08-26 01:24:48 -0400

    Don't set properties that will be set later anyway

--- src/utils/hitbox.js
+++ src/utils/hitbox.js
@@ -1,8 +1,8 @@
 class Hitbox {
     constructor() {
         this.position = {x: 0, y: 0};
-        this.radius = 0;
+        // this.radius = 0;
         this.lastCollisionAge = -9;
-        this.lastCollisionSegment = null;
+        // this.lastCollisionSegment = null;
     }
 }
