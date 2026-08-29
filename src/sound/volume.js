3dc1bdf84f7672dc5042e80c3debc1a89c9a277d
Remi Vansteelandt <remvst@gmail.com>
2026-08-29 13:27:06 -0400

    Add volume prompt when changing volume

--- src/sound/volume.js
+++ src/sound/volume.js
@@ -6,5 +6,6 @@ updateVolume = () => zzfxM_GAIN.gain.value = globalVolume() / VOLUME_PRECISION;
 cycleVolume = () => {
     writeLocalStorage('v', (globalVolume() + VOLUME_PRECISION) % (100 + VOLUME_PRECISION));
     updateVolume();
+    G?.screens[0]?.level?.world?.add(new Prompt(nomangle('Volume: ') + globalVolume() + '%')).removeWhenAgeIs(1);
 }
 updateVolume();
